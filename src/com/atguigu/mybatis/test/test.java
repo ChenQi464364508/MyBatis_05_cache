9d51c941402e72c2a19b96680bf4319a6a1bb24e
chenqi <464364508@qq.com>
2019-09-15 22:22:22 +0800

    Initial commit

---
+++ src/com/atguigu/mybatis/test/test.java
@@ -0,0 +1,14 @@
+package com.atguigu.mybatis.test;
+
+import org.junit.Test;
+
+/**
+ * @author Chandler_Chen
+ * @created 2019/9/15
+ */
+public class test {
+    @Test
+    public void te(){
+        System.out.println("yeste ");
+    }
+}
